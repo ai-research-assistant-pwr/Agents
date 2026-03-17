81f777e78ceb5a06d8413a13b4b109b93bf71d7d
tymson <tympex59@gmail.com>
2026-03-17 22:00:58 +0100

    h

--- sh_scripts/embed_prompts.sh
+++ sh_scripts/embed_prompts.sh
@@ -12,7 +12,7 @@
 source /usr/local/sbin/modules.sh
 module load Python/3.12.3-GCCcore-13.3.0
 
-source ~/disk/venvs/pnw-2/bin/activate 
+source ~/disk/venvs/pnw/bin/activate 
 
 GET_CONFIG="python3 Agents/src/utils/config.py"
 
