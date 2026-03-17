1218d9ca4dc85d615b2d61344515011412ca861f
tymson <tympex59@gmail.com>
2026-03-17 23:26:51 +0100

    sh fix 2

--- sh_scripts/embed_prompts.sh
+++ sh_scripts/embed_prompts.sh
@@ -12,12 +12,18 @@
 source /usr/local/sbin/modules.sh
 module load Python/3.12.3-GCCcore-13.3.0
 
-source ~/disk/venvs/pnw-2/bin/activate 
+source /home/tymrom7227/disk/venvs/pnw-2/bin/activate
 
-GET_CONFIG="~/disk/venvs/pnw-2/bin/python Agents/src/utils/config.py"
+VENV_PYTHON="/home/tymrom7227/disk/venvs/pnw-2/bin/python"
+GET_CONFIG="$VENV_PYTHON Agents/src/utils/config.py"
 
-MY_DISK=$($GET_CONFIG paths.base_path)
-PROMPTS_DIR=$MY_DISK/$($GET_CONFIG paths.prompts_dir) 
+MY_DISK="/lustre/pd03/hpc-patswi3426-1763133915/Agents"
+
+PROMPTS_DIR_RELATIVE=$($GET_CONFIG paths.prompts_dir)
+PROMPTS_DIR="$MY_DISK/$PROMPTS_DIR_RELATIVE"
+
+export XDG_CACHE_HOME=$MY_DISK/.cache
+export HOME=$MY_DISK
 
 export TORCHINDUCTOR_CACHE_DIR=$MY_DISK/.cache/torch_inductor
 export VLLM_CACHE_ROOT=$MY_DISK/.cache/vllm
@@ -25,7 +31,6 @@ export HF_HOME=$MY_DISK/.cache/hf
 export TRITON_CACHE_DIR=$MY_DISK/.cache/triton
 export FLASHINFER_CACHE_DIR=$MY_DISK/.cache/flashinfer
 export TORCH_EXTENSIONS_DIR=$MY_DISK/.cache/torch_extensions
-export XDG_CACHE_HOME=$MY_DISK/.cache
 
 export HF_HUB_OFFLINE=1
 
@@ -35,12 +40,12 @@ echo "  VLLM Compile Cache: $VLLM_CACHE_ROOT"
 echo "  Prompts Path: $PROMPTS_DIR"
 
 rm -rf "$VLLM_CACHE_ROOT"
-mkdir -p "$VLLM_CACHE_ROOT"
-mkdir -p "$PROMPTS_DIR"
+mkdir -p $MY_DISK/.cache/vllm
+mkdir -p $MY_DISK/.cache/torch_inductor
 
 echo "====================================="
 echo "Starting Prompts Embedding..."
 echo "====================================="
 
-~/disk/venvs/pnw-2/bin/python Agents/src/embed_prompts.py
+$VENV_PYTHON $MY_DISK/src/embed_prompts.py
 echo "Done!"
