d06b067fe6f1c1a165e0bfeef01f5d0298e97e0a
tymson <tympex59@gmail.com>
2026-03-17 21:19:57 +0100

    h

--- sh_scripts/embed_prompts.sh
+++ sh_scripts/embed_prompts.sh
@@ -10,10 +10,11 @@
 #SBATCH --gres=gpu:hopper:1
 
 source /usr/local/sbin/modules.sh
+module load Python/3.12.3-GCCcore-13.3.0
 
 source ~/disk/venvs/pnw-2/bin/activate 
 
-GET_CONFIG="python Agents/src/utils/config.py"
+GET_CONFIG="python3 Agents/src/utils/config.py"
 
 MY_DISK=$($GET_CONFIG paths.base_path)
 PROMPTS_DIR=$MY_DISK/$($GET_CONFIG paths.prompts_dir) 
