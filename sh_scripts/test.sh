0f26f9d7895088023305d783daf4995967a80bad
tymson <tympex59@gmail.com>
2026-03-19 12:51:19 +0100

    test fix

--- sh_scripts/test.sh
+++ sh_scripts/test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH --mem=32gb
+#SBATCH --time=0-00:30:00
+#SBATCH --job-name=dense_retrieval
+#SBATCH --output=Agents/out/retrieval.out
+#SBATCH -p lem-gpu-short
+#SBATCH --gres=gpu:hopper:1
+
 source ~/disk/venvs/pnw-2/bin/activate
 
 MY_DISK="$HOME/disk"
