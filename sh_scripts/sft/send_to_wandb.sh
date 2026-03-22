56c4be486bf7d12bc3c0c6b43dcf36ba3e07e981
tymson <tympex59@gmail.com>
2026-03-22 15:55:12 +0100

    script for sending logs to wandb

---
+++ sh_scripts/sft/send_to_wandb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+WANDB_API_KEY=$1
+RELATIVE_LOG_PATH=$2
+
+if [ -z "$WANDB_API_KEY" ] || [ -z "$RELATIVE_LOG_PATH" ]; then
+    echo "Error: Missing arguments."
+    echo "Usage: bash sync_manual_wandb.sh YOUR_API_KEY path/to/log"
+    echo "Example: bash sync_manual_wandb.sh [API_KEY] disk/wandb/offline-run-20260322_..."
+    exit 1
+fi
+
+source /usr/local/sbin/modules.sh
+module load Python/3.12.3-GCCcore-13.3.0
+source /home/tymrom7227/disk/venvs/pnw-2/bin/activate
+
+export WANDB_API_KEY=$WANDB_API_KEY
+
+FULL_PATH="$HOME/$RELATIVE_LOG_PATH"
+
+if [ ! -d "$FULL_PATH" ]; then
+    echo "Error: Directory does not exist: $FULL_PATH"
+    echo "Make sure the path is correct (e.g., starts with 'disk/wandb/...')"
+    exit 1
+fi
+
+echo "Synchronizing: $FULL_PATH"
+echo "-------------------------------------------------"
+
+python -m wandb sync "$FULL_PATH"
+
+echo "-------------------------------------------------"
+echo "Done!"
