58fc48debd9b7a68e64638cd6dea948ca70fa1d0
tymson <tympex59@gmail.com>
2026-04-20 17:08:51 +0200

    updated grpo param train

--- sh_scripts/grpo/run_grpo.sh
+++ sh_scripts/grpo/run_grpo.sh
@@ -33,6 +33,7 @@ export XDG_CACHE_HOME=$MY_DISK/.cache
 export HF_HOME=$MY_DISK/.cache/hf
 export TRANSFORMERS_OFFLINE=0
 export NCCL_DEBUG=WARN
+export TRITON_CACHE_DIR="$MY_DISK/.cache/triton" 
 
 # =================================================
 # FINAL CHECKS
@@ -53,40 +54,38 @@ OUTPUT_DIR="$MY_DISK/models_output/grpo_results"
 export MARTI_CONFIG_PATH="$AGENTS_DIR/config.yaml"
 
 if [ ! -z "$WANDB_API_KEY" ]; then
-    WANDB_FLAG="--use_wandb $WANDB_API_KEY --wandb_project MARTI_GRPO --wandb_run_name grpo_exp_1"
+    WANDB_FLAG="--logger.wandb.key $WANDB_API_KEY --logger.wandb.project MARTI_GRPO --logger.wandb.run_name grpo_exp_1"
 fi
 
 $VENV_PYTHON -m openrlhf.cli.train_ppo_ray \
-    --pretrain $BASE_MODEL \
-    --adapter_path $SFT_ADAPTER \
-    --save_path $OUTPUT_DIR \
-    --agent_func_path $AGENT_ENV_SCRIPT \
-    --prompt_data "json@$DATA_PATH" \
-    --input_key "query" \
-    --label_key "expected_action" \
-    --observation_key "visible_chunks" \
-    --advantage_estimator "group_norm" \
-    --colocate_all_models \
-    --vllm_num_engines 1 \
-    --vllm_tensor_parallel_size 1 \
-    --vllm_gpu_memory_utilization 0.5 \
-    --vllm_enable_sleep \
-    --deepspeed_enable_sleep \
-    --enforce_eager \
-    --ref_num_nodes 1 \
-    --ref_num_gpus_per_node 1 \
-    --actor_num_nodes 1 \
-    --actor_num_gpus_per_node 1 \
-    --actor_learning_rate 5e-7 \
-    --train_batch_size 16 \
-    --micro_train_batch_size 1 \
-    --rollout_batch_size 16 \
-    --n_samples_per_prompt 4 \
-    --max_epochs 1 \
-    --prompt_max_len 2048 \
-    --generate_max_len 256 \
-    --zero_stage 2 \
-    --bf16 \
-    --gradient_checkpointing \
-    --save_hf_ckpt \
+    --actor.model_name_or_path $SFT_ADAPTER \
+    --ckpt.output_dir $OUTPUT_DIR \
+    --train.agent_func_path $AGENT_ENV_SCRIPT \
+    --data.prompt_dataset "json@$DATA_PATH" \
+    --data.input_key "query" \
+    --data.label_key "expected_action" \
+    --algo.advantage.estimator "group_norm" \
+    --train.colocate_all \
+    --vllm.num_engines 1 \
+    --vllm.tensor_parallel_size 1 \
+    --vllm.gpu_memory_utilization 0.5 \
+    --vllm.enable_sleep \
+    --ds.enable_sleep \
+    --vllm.enforce_eager \
+    --ref.num_nodes 1 \
+    --ref.num_gpus_per_node 1 \
+    --actor.num_nodes 1 \
+    --actor.num_gpus_per_node 1 \
+    --actor.adam.lr 5e-7 \
+    --train.batch_size 16 \
+    --train.micro_batch_size 1 \
+    --rollout.batch_size 16 \
+    --rollout.n_samples_per_prompt 4 \
+    --train.max_epochs 1 \
+    --data.max_len 2048 \
+    --rollout.max_new_tokens 256 \
+    --ds.zero_stage 2 \
+    --ds.param_dtype bf16 \
+    --actor.gradient_checkpointing_enable \
+    --ckpt.save_hf \
     $WANDB_FLAG
