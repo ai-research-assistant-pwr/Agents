18af9c273b8e49419464367c349bb7b0acc1a99a
Patryk Świat <patswi3426@ui.wcss.pl>
2026-05-03 19:51:07 +0200

    beter sun scrupt

--- sh_scripts/grpo/new_run_grpo.sh
+++ sh_scripts/grpo/new_run_grpo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -c 8
+#SBATCH -c 32
 #SBATCH --mem=128gb
-#SBATCH --time=0-00:05:00
+#SBATCH --time=0-00:10:00
 #SBATCH --job-name=grpo_qwen
 #SBATCH --output=/home/patswi3426/disk/patryk/Agents/out/grpo_qwen.out
 #SBATCH -p lem-gpu-short
-#SBATCH --gres=gpu:hopper:3
+#SBATCH --gres=gpu:hopper:2
 
 set -e 
 
@@ -31,6 +31,11 @@ export TRITON_CACHE_DIR="$MY_NEW_TMP/triton_cache"
 export TORCHINDUCTOR_CACHE_DIR="$MY_NEW_TMP/torchinductor_cache"
 export VLLM_USE_V1="0"
 export VLLM_WORKER_MULTIPROC_METHOD="spawn"
+
+mkdir -p "$MY_NEW_TMP"
+mkdir -p "$XDG_CACHE_HOME"
+mkdir -p "$TRITON_CACHE_DIR"
+mkdir -p "$TORCHINDUCTOR_CACHE_DIR"
 
 # =================================================
 # START TRAINING
@@ -83,7 +88,7 @@ $VENV_PYTHON -m marti.cli.multi_agent_train_ppo_ray \
     --label_key "hypothesis" \
     --metadata_key "metadata" \
     --advantage_estimator "group_norm" \
-    --vllm_num_engines 1 \
+    --vllm_num_engines 2 \
     --vllm_tensor_parallel_size 1 \
     --vllm_gpu_memory_utilization 0.6 \
     --colocate_all_models \
@@ -92,17 +97,17 @@ $VENV_PYTHON -m marti.cli.multi_agent_train_ppo_ray \
     --vllm_enable_sleep \
     --deepspeed_enable_sleep \
     --actor_num_nodes 1 \
-    --actor_num_gpus_per_node 1 \
+    --actor_num_gpus_per_node 2 \
     --ref_num_nodes 1 \
-    --ref_num_gpus_per_node 1 \
+    --ref_num_gpus_per_node 2 \
     --lr_scheduler constant \
     --actor_learning_rate 5e-7 \
     --use_kl_loss \
     --init_kl_coef 0.05 \
-    --train_batch_size 4 \
+    --train_batch_size 8 \
     --micro_train_batch_size 1 \
-    --rollout_batch_size 4 \
-    --n_samples_per_prompt 8 \
+    --rollout_batch_size 8 \
+    --n_samples_per_prompt 16 \
     --num_episodes 5 \
     --max_epochs 1 \
     --prompt_max_len 8096 \
