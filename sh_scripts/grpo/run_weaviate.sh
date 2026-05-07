64e8dea483a69bbe48d50ac6099cfd26bc94285e
Patryk Swiat <patrykswiat@gmail.com>
2026-05-07 20:03:45 +0200

    update weaviate sh

--- sh_scripts/grpo/run_weaviate.sh
+++ sh_scripts/grpo/run_weaviate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -p lem-cpu-short
+#SBATCH -p bem2-cpu-short
 #SBATCH --job-name=weaviate_test
 #SBATCH --output=out/weaviate_%j.log
 #SBATCH --nodes=1
@@ -15,10 +15,10 @@ echo "Uruchamianie Weaviate (metoda RUN)..."
 # Używamy dokładnie tych samych flag, które zadziałały w srun
 apptainer run --contain \
     --no-home \
-    --bind "$BASE_DIR/weaviate_data:/var/lib/weaviate" \
+    --bind "$BASE_DIR/data/research-paper-embed-qwen-4b:/var/lib/weaviate" \
     --env "AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED=true" \
     --env "PERSISTENCE_DATA_PATH=/var/lib/weaviate" \
-    --env "CLUSTER_HOSTNAME=localhost" \
+    --env "CLUSTER_HOSTNAME=node1" \
     weaviate.sif &
 
 # Zapisujemy PID procesu, żeby SLURM go widział
@@ -30,6 +30,7 @@ sleep 30
 # Test lokalny (wewnątrz skryptu sbatch)
 echo "Testowanie połączenia lokalnie na węźle..."
 curl -s http://localhost:8080/v1/meta
+curl -s http://localhost:8080/v1/schema
 
 # Trzymamy zadanie przy życiu
 wait $WEAVIATE_PID
