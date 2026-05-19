a28eb6324a455f5459cf82f9d4e8017af082999c
tymson <tympex59@gmail.com>
2026-05-19 15:42:28 +0200

    another path fix

--- src/grpo/grpo_train/mege_lora.py
+++ src/grpo/grpo_train/mege_lora.py
@@ -3,15 +3,18 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-adapter_path = os.path.join(BASE_DIR, "models_output", "sft", "lora_shared_agent_Qwen3-4B")
-output_path = os.path.join(BASE_DIR, "models_output", "merged_sft_qwen_4B")
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-print("Ładowanie modelu bazowego...")
+ROOT_DIR = os.path.abspath(os.path.join(THIS_DIR, "..", "..", "..", ".."))
+
+adapter_path = os.path.join(ROOT_DIR, "models_output", "sft", "lora_shared_agent_Qwen3-4B")
+output_path = os.path.join(ROOT_DIR, "models_output", "merged_sft_qwen_4B")
+
+print("Ładowanie modelu bazowego na CPU...")
 base_model = AutoModelForCausalLM.from_pretrained(
     "Qwen/Qwen3-4B",
-    torch_dtype=torch.float16,
-    device_map="auto",
+    torch_dtype=torch.bfloat16,
+    device_map="cpu"
 )
 print("Aplikowanie adaptera LoRA...")
 model = PeftModel.from_pretrained(base_model, adapter_path)
