8030b93d1a42731aa2c24e8dd9c7d9547f0d020c
tymson <tympex59@gmail.com>
2026-05-19 15:34:26 +0200

    path fix

--- src/grpo/grpo_train/mege_lora.py
+++ src/grpo/grpo_train/mege_lora.py
@@ -2,14 +2,16 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 
-base_model_id = "Qwen/Qwen3-4B"
-# Ścieżka, gdzie Twój run_sft.py zrzucił adapter
-adapter_path = "./models_output/sft/lora_shared_agent_Qwen3-4B" 
-output_path = "./models_output/merged_sft_qwen_4B"
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+adapter_path = os.path.join(BASE_DIR, "models_output", "sft", "lora_shared_agent_Qwen3-4B")
+output_path = os.path.join(BASE_DIR, "models_output", "merged_sft_qwen_4B")
 
 print("Ładowanie modelu bazowego...")
 base_model = AutoModelForCausalLM.from_pretrained(
-    base_model_id, torch_dtype=torch.bfloat16, device_map="cpu"
+    "Qwen/Qwen3-4B",
+    torch_dtype=torch.float16,
+    device_map="auto",
 )
 print("Aplikowanie adaptera LoRA...")
 model = PeftModel.from_pretrained(base_model, adapter_path)
@@ -19,6 +21,6 @@ model = model.merge_and_unload()
 
 print(f"Zapisywanie zmergowanego modelu do {output_path}...")
 model.save_pretrained(output_path)
-tokenizer = AutoTokenizer.from_pretrained(base_model_id)
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-4B")
 tokenizer.save_pretrained(output_path)
 print("Gotowe!")
