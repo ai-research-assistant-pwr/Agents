4f3742eb13d43cd44cbfee1c503c6e047b4e2321
tymson <tympex59@gmail.com>
2026-05-20 13:09:12 +0200

    more time

--- sh_scripts/grpo/run_grpo.sh
+++ sh_scripts/grpo/run_grpo.sh
@@ -4,7 +4,7 @@
 # =================================================
 #SBATCH --job-name=grpo_qwen
 #SBATCH --output=Agents/out/%x_%j.out
-#SBATCH --time=0-06:00:00
+#SBATCH --time=0-08:00:00
 #SBATCH -p lem-gpu-short
 #SBATCH -N 1
 #SBATCH -c 32
